28a3835366353a764f51f58a06affa93a28b31ea
Tom Pierce <tompierce4@gmail.com>
2024-02-19 17:01:24 +0000

    create refreshToken on user creation

--- models/User.js
+++ models/User.js
@@ -4,8 +4,20 @@ const Schema = mongoose.Schema;
 const userSchema = new Schema({
   email: { type: String, required: true },
   password: { type: String },
+  refreshToken: { type: String },
   accountType: { type: String, required: true },
   positions: [{ type: Schema.Types.ObjectId, ref: "Position" }],
+});
+
+userSchema.pre("save", () => {
+  this.refreshToken = jwt.sign(
+    { _id: user._id },
+    process.env.REFRESH_TOKEN_SECRET,
+    {
+      // thirty days
+      expiresIn: 60 * 60 * 24 * 30,
+    }
+  );
 });
 
 module.exports = mongoose.model("User", userSchema);
