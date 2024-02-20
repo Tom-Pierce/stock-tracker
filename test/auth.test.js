fe1f2d20a5e70bb748f28fb8100ad4ab2fd72de6
Tom Pierce <tompierce4@gmail.com>
2024-02-20 12:08:45 +0000

    user authentication test

--- test/auth.test.js
+++ test/auth.test.js
@@ -1,13 +1,20 @@
 const request = require("supertest");
 const app = require("../app");
+const User = require("../models/User");
 
 describe("API tests", () => {
-  it("should respond with 404 and errors if user email or password do not fit requirements", async () => {
+  beforeAll(async () => {
+    // clear database from previous tests
+    await User.deleteMany({}).exec();
+  });
+
+  it("should respond with 400 and errors if user email or password do not fit requirements", async () => {
     const res = await request(app).post("/api/auth/local/signup").send({
       email: "thisisnotavalidemail",
       password: "pwd",
       confirmPassword: "notTheSamePwd",
     });
+
     expect(res.status).toEqual(400);
     expect(res.body).toEqual([
       "Please provide a valid email",
@@ -16,5 +23,54 @@ describe("API tests", () => {
       "Password must contain a number",
       "Passwords don't match",
     ]);
+  });
+
+  it("should create a user and respond with 201 and a message", async () => {
+    const res = await request(app).post("/api/auth/local/signup").send({
+      email: "example@gmail.com",
+      password: "ThisIsAPassword123",
+      confirmPassword: "ThisIsAPassword123",
+    });
+
+    expect(res.status).toEqual(201);
+
+    const cookies = res.headers["set-cookie"];
+    expect(cookies).toContainEqual(expect.stringContaining("jwt="));
+    expect(cookies).toContainEqual(expect.stringContaining("refreshToken="));
+  });
+
+  it("should respond with 400 if email is already in use", async () => {
+    const res = await request(app).post("/api/auth/local/signup").send({
+      email: "example@gmail.com",
+      password: "ThisIsAPassword123",
+      confirmPassword: "ThisIsAPassword123",
+    });
+
+    expect(res.status).toEqual(400);
+    expect(res.body).toContainEqual(
+      expect.stringContaining("Email is already in use")
+    );
+  });
+
+  it("should respond with 401 if user attempts to login with incorrect email", async () => {
+    const res = await request(app).post("/api/auth/local/login").send({
+      email: "wrong@gmail.com",
+      password: "ThisIsAPassword123",
+    });
+
+    expect(res.status).toEqual(401);
+    expect(res.body.message).toEqual(
+      "No account found with that email address"
+    );
+  });
+
+  it("should respond with 401 if user attempts to login with incorrect password", async () => {
+    const res = await request(app).post("/api/auth/local/login").send({
+      email: "example@gmail.com",
+      password: "wrongpassword",
+    });
+
+    expect(res.status).toEqual(401);
+    expect(res.body.message).toEqual("Email or password incorrect");
   });
 });
