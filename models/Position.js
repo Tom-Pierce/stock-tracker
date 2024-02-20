b8e4559bb088fcfd84c31fa20e5df9db7908755a
Tom Pierce <tompierce4@gmail.com>
2024-02-20 21:03:21 +0000

    virtual value for total position value

--- models/Position.js
+++ models/Position.js
@@ -13,12 +13,22 @@ const positionSchema = new Schema({
   user: { type: Schema.Types.ObjectId, ref: "User", required: true },
 });
 
+// value of each seperate lot
 positionSchema
   .path("lots")
   .schema.virtual("value")
   .get(function () {
     return this.quantity * this.price;
   });
+
+//value of position
+positionSchema.virtual("value").get(function () {
+  let totalValue = 0;
+  this.lots.forEach((lot) => {
+    totalValue += lot.quantity * lot.price;
+  });
+  return totalValue;
+});
 
 positionSchema.set("toJSON", { virtuals: true });
 
