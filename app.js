2f41a2884b6bd137e11401d0428498996a89ff1e
Tom Pierce <tompierce4@gmail.com>
2024-02-21 13:06:16 +0000

    require cors

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
+const cors = require("cors");
 const mongoose = require("mongoose");
 require("dotenv").config();
 require("./utils/auth");
